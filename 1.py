6bbedaeaafb9dae0eab706f176859b8e9b788307
Pato-el-pato <luiseduardo.mena22@gmail.com>
2020-11-21 11:23:43 -0600

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,84 @@
+import os
+os.system('clear')
+#es para que quite la cosa que nadie nesesita
+
+#variables:
+##
+full_name = "Pato_el_pato"
+a = 1
+b = 2
+c = ["d","e","f"]
+g = ("t","e")
+h = {
+"p1": "t1",
+"p2": "t2",
+"p3": "t3"	
+}
+p6 = True 
+##
+print(full_name)
+
+print("commands print")
+
+print("hola")
+#es para que diga cualquier cosa
+
+print(a)
+
+print("list")
+
+print(c[0])
+print(c[1])
+print(c[2])
+
+print("tubble")
+
+print(g[0])
+print(g[1])
+
+print("dicionary")
+
+print(h["p1"])
+print(h["p2"])
+print(h["p3"])
+
+print("if commands")
+
+if a < b:
+	print("b es mas grande que a")
+if a > b:
+	print("a es mas grande que b")
+
+print("prueba")
+
+p4 = 10
+p5 = 15
+h = p4
+
+print(p4)
+print(p5)
+
+print("trancision")
+
+p4 = p5
+p5 = h
+
+print(p4)
+print(p5)
+
+print("trancision")
+
+p5 = p4
+p4 = h
+
+print(p4)
+print(p5)
+
+print("fin de la prueba")
+
+print(p6)
+
+if p6 is True:
+	print("awas")
+if p6 is False:
+	print("owos")
---
+++ 2.py
@@ -0,0 +1,8 @@
+#variables:
+##
+full_name = "Pato_el_pato"
+a = 1
+b = 2
+c =["d","e","f"]
+g = ("t","e")
+h =
